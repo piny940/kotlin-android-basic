3ae21d2a59edabce3db1cd2aed0d39a6415ae761
piny940 <an.s.shoma@gmail.com>
2023-10-31 18:52:58 +0900

    リストをactivityから表示する

--- ListViewSample2/app/build.gradle.kts
+++ ListViewSample2/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.listviewsample2"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.listviewsample2"
--- ListViewSample2/app/src/main/java/com/example/listviewsample2/MainActivity.kt
+++ ListViewSample2/app/src/main/java/com/example/listviewsample2/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.listviewsample2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val lvMenu = findViewById<ListView>(R.id.lvMenu)
+        val menuList = mutableListOf("唐揚げ定食", "ハンバーグ定食")
+        val adapter = ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, menuList)
+        lvMenu.adapter = adapter
     }
 }
