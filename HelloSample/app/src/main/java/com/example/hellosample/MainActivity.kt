8f8ffbc0bd5b210c4712136ac5516cdae4c9df30
piny940 <an.s.shoma@gmail.com>
2023-10-31 17:35:28 +0900

    HelloListenerを作成

--- HelloSample/app/src/main/java/com/example/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/com/example/hellosample/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.hellosample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonEl = findViewById<Button>(R.id.btClick)
+        buttonEl.setOnClickListener(HelloListener())
+    }
+
+    private inner class HelloListener : View.OnClickListener {
+        override fun onClick(view: View?) {
+            val input = findViewById<EditText>(R.id.etName)
+            val output = findViewById<TextView>(R.id.tvOutput)
+            val inputStr = input.text.toString()
+            output.text = inputStr + "さん、こんにちは！"
+        }
     }
 }
